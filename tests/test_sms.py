34eb60ccefb8d4d2c9a8b179be1ea1510dd0de4a
Kevan Ahlquist <ahlqu039@umn.edu>
2014-06-28 00:47:46 -0500

    Changed max message length to 130.

--- tests/test_sms.py
+++ tests/test_sms.py
@@ -10,28 +10,45 @@ from dci_notify.sms import split_msg, send_sms
 class TestSplitMessage:
 
     def test_split_msg_one_chunk(self):
-        short_msg = 'a' * 150
-        chunks = split_msg(short_msg)
+        msg = 'a' * 130
+        chunks = split_msg(msg)
         assert len(chunks) is 1
 
     def test_split_msg_multi_chunk(self):
-        short_msg = 'a' * 500
-        chunks = split_msg(short_msg)
+        msg = 'a' * 500
+        chunks = split_msg(msg)
         assert len(chunks) is 4
+
+    def test_split_msg_line_breaks(self):
+        msg = 'a' * 130 + '\n' + 'b' * 40
+        chunks = split_msg(msg)
+        assert len(chunks) is 2
+        assert len(chunks[0]) == 130
+
+    def test_split_msg_one_line(self):
+        msg = 'a' * 160 + 'b' * 20
+        chunks = split_msg(msg)
+        assert len(chunks) is 2
 
 
 class TestSendMessage:
 
     def test_send_sms_single_message(self, app):
         with mail.record_messages() as outbox:
-            send_sms('verizon', 5551112222, 'message', 'subject')
+            send_sms(carrier='verizon',
+                     number=5551112222,
+                     message='message',
+                     subject='subject')
             assert len(outbox) is 1
             assert outbox[0].subject == 'subject'
             assert outbox[0].body == 'message'
 
     def test_send_sms_multiple_messages(self, app):
         with mail.record_messages() as outbox:
-            send_sms('verizon', 5551112222, 'm' * 300, 'subject')
-            assert len(outbox) is 2
+            send_sms(carrier='verizon',
+                     number=5551112222,
+                     message='m' * 300,
+                     subject='subject')
+            assert len(outbox) is 3
             assert outbox[0].subject == 'subject'
-            assert outbox[0].body == 'm' * 159
+            assert outbox[0].body == 'm' * 130
