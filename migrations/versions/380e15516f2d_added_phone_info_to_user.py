adf91d24ca27b5c4c1f5c7b833c822b731e30af1
Kevan Ahlquist <ahlqu039@umn.edu>
2015-06-07 04:03:13 -0500

    Fixed db migration error, Carriers type.

--- migrations/versions/380e15516f2d_added_phone_info_to_user.py
+++ migrations/versions/380e15516f2d_added_phone_info_to_user.py
@@ -13,8 +13,13 @@ down_revision = 'd4b786027fd'
 from alembic import op
 import sqlalchemy as sa
 
+from dci_notify.user.models import User
+
 
 def upgrade():
+    bind = op.get_bind()
+    typ = User.__table__.c.carrier.type
+    typ._on_table_create(User.__table__, bind, checkfirst=True)
     op.add_column('users', sa.Column('carrier', sa.Enum('suncom', 'nextel', 'powertel', 'bellsouthmobility', 'sprint', 'telus_mobility', 'virgin', 'comcast', 'southernlink', 'boost', 'pscwireless', 'cricket', 'metropcs', 'tracfone', 'verizon', 'bell_atlantic', 'uscellular', 'qwest', 'kajeet', 't_mobile', 'at&t', 'cellularsouth', 'alltel', 'blueskyfrog', 'ameritech', name='Carriers'), nullable=True))
     op.add_column('users', sa.Column('phone_active', sa.Boolean(), nullable=True))
     op.add_column('users', sa.Column('phone_num', sa.String(length=10), nullable=True))
